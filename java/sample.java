3f464f1ac2968dded0561d47a78ca15af24e613b
Ravidemo123 <raviops100@gmail.com>
2025-08-07 02:46:44 +0530

    java

--- java/sample.java
+++ java/sample.java
@@ -2,7 +2,5 @@ public class MultiplePrints {
     public static void main(String[] args) {
         // Printing multiple statements
         System.out.println("This is the first print statement.");
-		System.out.println("denamma jeevitham.");
-        
     }
 }
