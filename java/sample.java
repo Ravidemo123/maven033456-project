c2791c9d9f25450b32796f0fc42d84569a8d1833
Ravidemo123 <raviops100@gmail.com>
2025-08-08 10:23:51 +0530

    removed java

--- java/sample.java
+++
@@ -1,7 +0,0 @@
-public class MultiplePrints {
-    public static void main(String[] args) {
-        // Printing multiple statements
-        System.out.println("This is the first print statement.");
-        
-    }
-}
